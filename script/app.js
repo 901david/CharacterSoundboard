c04bc87a5c0604a1550d52feb851c5f16010053a
h fam <h fam>
2017-06-13 12:56:30 -0600

    Made layout look better. Got fake buttons working and reacting with audio and visual links that can be utilized to switch out the images. Need to Tighten up the jumbo, find a better top font and move forward with animating.

--- script/app.js
+++ script/app.js
@@ -1,15 +1,28 @@
-var soundsLeft = ["button1", "button2", "button3", "button4"];
-var soundsRight = ["button5", "button6", "button7", "button8"];
-
+var soundLeft = 
+[{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"}, {"button": "title", "gifLink": "Link", "soundLink": "SoundURL"},{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"},{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"}];
+var soundRight = [{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"},{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"},{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"},{"button": "title", "gifLink": "Link", "soundLink": "SoundURL"}];
+var gifChosen;
+var audioChosen;
 // This function creates buttons
-function placeButtons (x){
-	for (var i = 0; i < x.length;i++) {
-		$("#" + x).append("<button class='btn btn-danger'>" + x[i] + "</button");
-		console.log(x[i]);
-		console.log($("#" + x));
+function placeButtonsLeft (){
+	for (var i = 0; i < soundLeft.length;i++) {
+		$("#soundLeft").append("<button class='btn btn-danger spaceGood' data-gif='" + soundLeft[i].gifLink + "' data-sound='" + soundLeft[i].soundLink + "'>" + soundLeft[i].button + "</button");
+	};
+};
+function placeButtonsRight (){
+	for (var i = 0; i < soundRight.length;i++) {
+		$("#soundRight").append("<button class='btn btn-danger spaceGood' data-gif='" + soundRight[i].gifLink + "' data-sound='" + soundRight[i].soundLink + "'>" + soundRight[i].button + "</button");
 	};
 };
 $(document).ready(function(){
-placeButtons(soundsLeft);
-placeButtons(soundsRight);
+placeButtonsLeft();
+placeButtonsRight();
+$(".spaceGood").click(function () {
+gifChosen = $(this).attr("data-gif");
+audioChosen = $(this).attr("data-sound");
+$("#mainImage").attr("src", gifChosen);
+$("#mainAudio").attr("src", audioChosen);
+console.log(gifChosen);
+console.log(audioChosen);
+});
 	});
